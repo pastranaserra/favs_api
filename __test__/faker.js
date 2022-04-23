4d1afe327663a5add03414e566c4a4eb1600356a
Nicolás Pastrana <pastrana.serra@gmail.com>
2022-04-22 19:38:03 -0500

    Includes basic integration testing

---
+++ __test__/faker.js
@@ -0,0 +1,29 @@
+const { faker } = require('@faker-js/faker');
+
+const getUser = function (overrides = {}) {
+  const name = faker.name.firstName();
+  const lastname = faker.name.lastName();
+  const user = {
+    name,
+    lastname,
+    email: faker.internet.email(name, lastname),
+    password: faker.internet.password(4),
+  };
+
+  return Object.assign(user, overrides);
+};
+
+const getFav = function (overrides = {}) {
+  const fav = {
+    title: faker.commerce.department(),
+    description: faker.lorem.lines(),
+    link: faker.internet.url(),
+  };
+
+  return Object.assign(fav, overrides);
+};
+
+module.exports = {
+  getUser,
+  getFav,
+};
---
+++ __test__/user-rsrc.spec.js
@@ -0,0 +1,91 @@
+const request = require('supertest');
+const { getUser, getFav } = require('./faker');
+
+const server = require('../server');
+const config = require('../server/config');
+const database = require('../server/database');
+
+beforeAll(() => {
+  //connect to de test database
+  const url = `${config.database.url}-test`;
+  database.connect({
+    url,
+  });
+});
+
+// afterAll(() => {
+//   //disconnet database
+//   database.disconnect();
+// });
+
+const app = request(server);
+const user = getUser();
+const fav = getFav();
+let token;
+let favId;
+let userId;
+
+describe('Users resources', () => {
+  test('Users can signup', async () => {
+    const res = await app.post('/api/users/signup').send(user);
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('Users can login', async () => {
+    const logInfo = { email: user.email, password: user.password };
+
+    const res = await app.post('/auth/local/login').send(logInfo);
+
+    token = res.body.meta.token;
+    userId = res.body.data.id;
+    expect(res.statusCode).toBe(200);
+  });
+});
+
+// --------------------------------------------------
+
+describe('Data manipulation', () => {
+  test('Users can get the list of favs lists', async () => {
+    const res = await app
+      .get('/api/favs')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('Users can create a favs list', async () => {
+    fav.userId = userId;
+    const res = await app
+      .post('/api/favs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(fav);
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('Users can update a single favs list', async () => {
+    const res = await app
+      .put(`/api/favs/${favId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send(getFav());
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('Users can get a single favs list', async () => {
+    const res = await app
+      .get(`/api/favs/${favId}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('Users can delete a single favs list', async () => {
+    const res = await app
+      .delete(`/api/favs/${favId}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(res.statusCode).toBe(200);
+  });
+});
